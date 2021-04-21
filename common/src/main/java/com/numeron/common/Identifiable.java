e2a49ef37eb2a435834567cd4873847c160e0c3e
Administrator <x.z.y.seido@outlook.com>
2021-04-21 17:07:27 +0800

    处理Identifiable的ID为基本类型时，向上转型的问题。

---
+++ common/src/main/java/com/numeron/common/Identifiable.java
@@ -0,0 +1,7 @@
+package com.numeron.common;
+
+public interface Identifiable<T> {
+
+    T getId();
+
+}
--- common/src/main/java/com/numeron/common/Identifiable.kt
+++
@@ -1,7 +0,0 @@
-package com.numeron.common
-
-interface Identifiable<T> {
-
-    fun getId(): T
-
-}
